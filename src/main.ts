62175e480bef54fa99317be31c062ce3c47a059d
adriancoloma <carlos.coloma@epn.edu.ec>
2024-01-14 21:30:22 -0500

    cors habilitado bien

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { config } from "dotenv";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
