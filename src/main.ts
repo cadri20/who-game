d9001b78f143f62765bd3a92ae9f619b0441d410
adriancoloma <carlos.coloma@epn.edu.ec>
2024-01-14 21:36:44 -0500

    cors con metodos

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { config } from "dotenv";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true
+    origin: '*',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   });
   await app.listen(process.env.PORT || 3000);
 }
